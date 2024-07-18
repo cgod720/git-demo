60e75782b01b23235f167c620d7ba1fcfbe6be28
Carlos Godoy <carlosgodoy@pursuit.org>
2024-07-18 10:16:56 -0400

    added another flavor

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // Hello world
 const colors = ['red', 'green', 'banana']
 
-const flavors = ["vanilla", "chocolate", "strawberry"]
+const flavors = ["vanilla", "chocolate", "strawberry", "rocky road"]
